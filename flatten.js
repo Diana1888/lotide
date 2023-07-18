b4d31a2a8457ae72b5e6e66ac8dc5ecf58f48675
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-07-18 01:16:40 -0400

    Added flatten function

---
+++ flatten.js
@@ -0,0 +1,52 @@
+const assertArraysEqual = function(arrOne, arrTwo,) {
+
+  if (arrOne.length !== arrTwo.length) {
+    console.log("🛑Assertion failed: arrays are NOT equal");
+    return;
+  }
+  
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      console.log("🛑Assertion failed: arrays are NOT equal");
+      return;
+    }
+  }
+  
+  console.log("✅Assertion passed: arrays are equal");
+  return;
+
+};
+
+
+const eqArrays = function(arrOne, arrTwo,) {
+
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  
+  return true;
+
+};
+
+const flatten = function(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    let element = arr[i];
+    if (!Array.isArray(element)) {
+      newArray.push(element);
+    }  else {
+      for (let j = 0; j < element.length; j++) {
+        newArray.push(element[j]);
+      }
+    }
+  }
+  return newArray;
+};
+
+flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
