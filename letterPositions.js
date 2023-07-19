9f90d340f7d0ca901c26071383fbebed97a1a457
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-07-19 16:57:39 -0400

    Added function that returns index of the character

---
+++ letterPositions.js
@@ -0,0 +1,49 @@
+const assertArraysEqual = function(arrOne, arrTwo,) {
+
+  if (arrOne.length !== arrTwo.length) {
+    console.log("🛑Assertion failed: arrays are NOT equal");
+    return;
+  }
+  
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      console.log("🛑Assertion failed: arrays are NOT equal");
+      return;
+    }
+  }
+  
+  console.log("✅Assertion passed: arrays are equal");
+  return;
+
+};
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+  
+  console.log(`🛑🛑🛑Assertion Failed: ${actual} !==  ${expected}`);
+};
+
+
+const letterPositions = function(sentence) {
+  const output = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (!output[sentence[i]]) {
+        output[sentence[i]] = [i];
+      } else {
+        output[sentence[i]].push(i);
+      }
+    }
+  }
+  return output;
+};
+
+console.log(letterPositions("hello"));
+assertArraysEqual(letterPositions("hello").e, [1]);
+console.log(letterPositions("lighthouse in the house"));
+assertArraysEqual(letterPositions("lighthouse in the house").t, [4, 14]);
