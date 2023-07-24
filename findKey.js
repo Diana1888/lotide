d1077ab858a1e7642dc34af91d3d47068772c954
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-07-24 00:44:56 -0400

    Added function to return first key when callback is truthy

---
+++ findKey.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+  
+  console.log(`🛑🛑🛑Assertion Failed: ${actual} !==  ${expected}`);
+};
+
+
+const findKey = function(obj, callback) {
+
+  for (const element of Object.keys(obj)) {
+    if (callback(obj[element])) {
+      return element;
+    }
+  }
+
+};
+
+const rating = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);// => "noma"
+
+assertEqual(findKey(rating, x => x.stars === 2), "noma"); // => "noma"
+assertEqual(findKey(rating, x => x.stars === 5), undefined); // => undefined
+assertEqual(findKey(rating, x => x.stars === 3), "Akaleri"); // => "Akaleri"
