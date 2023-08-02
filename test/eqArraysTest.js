563deb99dba05c520ac6215d028dba96b595cded
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-08-02 13:54:53 -0400

    Separated tests from eqArrays file

---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+//TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 3, 3]), false); // => should FAIL
+assertEqual(eqArrays([1, 2], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
